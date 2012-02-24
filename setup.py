4f704bdc19d05275f1969da685c0f03e4adac0f7
ushi <ushi@porkbox.net>
2012-02-24 17:40:32 +0100

    1.3.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='local-repo',
-      version='1.333',
+      version='1.3.1',
 	  description='Arch Linux local repository manager',
 	  author='ushi',
 	  author_email='ushi@porkbox.net',

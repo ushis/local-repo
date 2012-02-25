fae27f6dbcbbffff1b126a83f0b1a9aeaa580483
ushi <ushi@porkbox.net>
2012-02-26 00:30:45 +0100

    added locale stuff to __init__.py

---
+++ localrepo/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.2
+
+from os.path import abspath, dirname, join, pardir
+
+import builtins
+import gettext
+
+__all__ = ['aur', 'msg', 'package', 'repo']
+
+dev_path = join(abspath(dirname(__file__)), pardir)
+
+if path.exists(join(dev_path, 'setup.py')):
+	gettext.bindtextdomain('localrepo', join(dev_path, 'share', 'locale'))
+
+gettext.textdomain('localrepo')
+builtins._ = gettext.gettext

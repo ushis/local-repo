1c1dec5efc689a0b50f2c334fd743f3171840e84
ushi <ushi@porkbox.net>
2012-02-28 11:19:20 +0100

    cleaned po.sh

--- share/po.sh
+++ share/po.sh
@@ -3,9 +3,9 @@
 usage() {
 	echo "usage: ${0} template|merge|compile"
 	echo ''
-	echo 'template  creates a po template from the source files'
-	echo 'merge     merges an updated template in to existing po files'
-	echo 'compile   compiles the po files'
+	echo 't, template  creates a po template from the source files'
+	echo 'm, merge     merges an updated template in to existing po files'
+	echo 'c, compile   compiles the po files'
 }
 
 if [ $# -ne 1 ]; then
@@ -15,41 +15,45 @@ fi
 
 cd $(dirname "$0")
 
+POT=messages.pot
+TRANS=translations
+MO=localrepo.mo
+
 case "$1" in
-	template)
-		echo -e '\e[1;33mGenerating:\e[0m messages.pot'
+	t|template)
+		echo -e "\e[1;33mGenerating:\e[0m ${POT}"
 		xgettext -o - -L Python ../local-repo ../localrepo/*.py |\
 			sed 's/\(charset=\)CHARSET/\1UTF-8/' |\
 			sed 's/SOME DESCRIPTIVE TITLE\./local-repo translation file/' |\
 			sed 's/\(Copyright (C)\).\+/\1 2012 ushi/' |\
 			sed 's/\(same license as the\) PACKAGE/\1 local-repo/' |\
-			sed 's/\(Project-Id-Version:\) PACKAGE VERSION/\1 1.4/' > messages.pot
+			sed 's/\(Project-Id-Version:\) PACKAGE VERSION/\1 1.4/' > "$POT"
 		;;
-	merge)
-		if [ ! -f messages.pot ]; then
-			echo -e '\e[1;31mNothing to merge:\e[0m messages.pot does not exist'
+	m|merge)
+		if [ ! -f "$POT" ]; then
+			echo -e "\e[1;31mNothing to merge:\e[0m ${POT} does not exist"
 			exit 1
 		fi
-		if [ ! -d translations ]; then
-			echo -e '\e[1;31mNothing to merge:\e[0m translations/ does not exist'
+		if [ ! -d "$TRANS" ]; then
+			echo -e '\e[1;31mNothing to merge:\e[0m ${TRANS}/ does not exist'
 			exit 1
 		fi
-		for f in $(ls translations/*.po); do
+		for f in $(ls $TRANS/*.po); do
 			echo -e "\e[1;33mUpdating:\e[0m ${f}"
-			msgmerge -UNq --backup=off "$f" messages.pot
+			msgmerge -UNq --backup=off "$f" "$POT"
 		done
 		;;
-	compile)
-		if [ ! -d translations ]; then
-			echo -e '\e[1;31mNothing to compile:\e[0m translations/ does not exist'
+	c|compile)
+		if [ ! -d "$TRANS" ]; then
+			echo -e "\e[1;31mNothing to compile:\e[0m ${TRANS} does not exist"
 			exit 1
 		fi
-		for f in $(ls translations/*.po); do
+		for f in $(ls $TRANS/*.po); do
 			echo -e "\e[1;33mCompiling:\e[0m ${f}"
 			lang=$(basename "$f" | cut -d'.' -f1)
 			dir="locale/${lang}/LC_MESSAGES"
 			[[ ! -d "$dir" ]] && mkdir -p "$dir"
-			msgfmt "$f" -o "${dir}/localrepo.mo"
+			msgfmt "$f" -o "${dir}/${MO}"
 		done
 		;;
 	*)

b630a7c112dda349a4cc00828f0f1df684021975
ushi <ushi@porkbox.net>
2012-02-29 18:45:26 +0100

    added pacman.py

---
+++ localrepo/pacman.py
@@ -0,0 +1,86 @@
+# pacman.py
+
+from os import chdir
+from os.path import exists, isdir
+from subprocess import call, check_output, CalledProcessError
+
+class PacmanError(Exception):
+	''' Handles programm call errors '''
+
+	def __init__(self, cmd):
+		''' Sets the failed command '''
+		self._cmd = cmd
+
+	def __str__(self):
+		return _('An error occurred while running: {0}').format(self._cmd)
+
+class Pacman:
+	''' A wrapper for program calls of the pacman package '''
+
+	#: Path to sudo
+	SUDO = '/usr/bin/sudo'
+
+	@staticmethod
+	def call(cmd):
+		''' Calls a command '''
+		if type(cmd) is str:
+			cmd = [cmd]
+
+		if call(cmd) is not 0:
+			raise PacmanError(' '.join(cmd))
+
+	@staticmethod
+	def install(pkgs, asdeps=False):
+		''' Installs packages '''
+		cmd = ['pacman', '-S'] + pkgs
+
+		if asdeps:
+			cmd += ['--asdeps']
+
+		if exists(Pacman.SUDO):
+			cmd.insert(0, 'sudo')
+		else:
+			cmd = ['su', '-c', '\''] + cmd + ['\'']
+
+		Pacman.call(cmd)
+
+	@staticmethod
+	def install_as_deps(pkgs):
+		''' Installs packages as dependencies '''
+		Pacman.install(pkgs, True)
+
+	@staticmethod
+	def check_deps(pkgs):
+		''' Checks for unresolved dependencies '''
+		unresolved = []
+
+		try:
+			check_ouput(['pacman', '-T'] + pkgs)
+		except CalledProcessError as e:
+			if e.returncode is 127:
+				unresolved += e.output.decode('utf8').split('\n')
+			else:
+				raise PacmanError('pacman')
+
+		return unresolved
+
+	@staticmethod
+	def make_package(path=None):
+		''' Calls makepkg '''
+		if path is not None:
+			if not isdir(path):
+				raise IOError(_('Could not find directory: {0}').format(path))
+
+			chdir(path)
+
+		Pacman.call(['makepkg', '-d'])
+
+	@staticmethod
+	def repo_add(db, pkgs):
+		''' Calls repo-add  '''
+		Pacman.call(['repo-add', db] + pkgs)
+
+	@staticmethod
+	def repo_remove(db, pkgs):
+		''' Calls repo-remove '''
+		Pacman.call(['repo-remove', db] + pkgs)

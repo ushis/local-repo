6c834d50116eb1f00df40c4cef8747a98ad84a19
ushi <ushi@porkbox.net>
2012-03-14 22:30:51 +0100

    nicer output

--- localrepo/msg.py
+++ localrepo/msg.py
@@ -11,27 +11,27 @@ class Msg:
 	@staticmethod
 	def process(*args):
 		''' Prints yellow bold process messages '''
-		print('\033[1;33m::', ' '.join(args), '\033[0m')
+		print('\033[0;34m>', ' '.join(args), '\033[0m')
 
 	@staticmethod
 	def error(*args):
 		''' Prints red bold error messages '''
-		print('\033[1;31m', ' '.join(args), '\033[0m', file=sys.stderr);
+		print('\033[1;31m' + ' '.join(args), '\033[0m', file=sys.stderr);
 
 	@staticmethod
 	def result(*args):
 		''' Prints blue bold result messages '''
-		print(' \033[1;34m', ' '.join(args), '\033[0m')
+		print('\033[1;34m' + ' '.join(args), '\033[0m')
 
 	@staticmethod
 	def info(*args):
 		''' Prints info messages '''
-		print('', ' '.join(args))
+		print(' '.join(args))
 
 	@staticmethod
 	def yes(*args):
 		''' Performs a simple yes/no question '''
-		a = input(' ' + ' '.join(args) + '? [y|N] ')
+		a = input(' '.join(args) + '? [y|N] ')
 		return False if re.match('^y(?:es)?', a, flags=re.IGNORECASE) is None else True
 
 	@staticmethod
@@ -94,4 +94,4 @@ class Msg:
 
 			nice.append((k, v))
 
-		return '\n '.join(('\033[0;36m{0:{1}}\033[0m  {2}'.format(k, max, v) for k, v in nice))
+		return '\n'.join(('\033[0;36m{0:{1}}\033[0m  {2}'.format(k, max, v) for k, v in nice))

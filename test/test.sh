3ae4ecbf849c1e721e5a83362c78d36fdfa8ec59
ushi <ushi@porkbox.net>
2012-03-29 20:12:19 +0200

    added unit tests for the parser class

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 # YEAH! Test them all..
-python -S *.py
+for f in *.py; do
+	echo ''
+	echo "Testing $f"
+	echo '----------------------------------------------------------------------'
+	python -S "$f"
+done

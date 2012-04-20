33bded601a6074149f593e8dbe8055a2fe6646c8
ushi <ushi@porkbox.net>
2012-04-20 17:51:31 +0200

    updated to version 1.6.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if call([join('share', 'po.sh'), 'compile']) is not 0:
 msg = glob(join('share', 'locale', '*', 'LC_MESSAGES', 'localrepo.mo'))
 
 setup(name='local-repo',
-      version='1.6.2',
+      version='1.6.3',
       description='Arch Linux local repository manager',
       author='ushi',
       author_email='ushi@porkbox.net',
